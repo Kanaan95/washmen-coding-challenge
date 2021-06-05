a1266d5b26acc19643fba28935a40c006ce4ce97
Rami Kanaan <kanaan95@live.com>
2021-06-05 12:47:38 +0400

    Added health check for node app

---
+++ healthcheck.js
@@ -0,0 +1,30 @@
+/**
+ * Health Check - Check that your app is running at a glance without digging into code
+ *
+ */
+
+// Dependencies
+const express = require("express");
+const router = express.Router({});
+
+/**
+ * @route           GET /api/healthCheck
+ * @description     Check that your app is running at a glance without digging into code
+ * @access          Public
+ */
+router.get("/", async (req, res) => {
+  const health = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.status(200).send(health);
+  } catch (err) {
+    health.message = err;
+    res.status(503).send();
+  }
+});
+
+// Export module
+module.exports = router;
