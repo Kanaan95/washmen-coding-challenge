fbd8079b3748893ccd8cf3ab79160b6b87407610
Rami Kanaan <kanaan95@live.com>
2021-06-06 20:54:06 +0400

    API Errors added error handling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MapsModule } from './maps/maps.module';
 import { DataService } from './services/data.service';
+import { AppErrorHandler } from './errors/app-error-handler';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { DataService } from './services/data.service';
     MapsModule,
     AppRoutingModule
   ],
-  providers: [DataService],
+  providers: [DataService, { provide: ErrorHandler, useClass: AppErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators'
 import { environment } from '../../environments/environment'
+import { AppError } from '../errors/app-error';
+import { BadRequest } from '../errors/bad-request';
+import { NotFoundError } from '../errors/not-found-error';
 import { Partners } from '../shared/models/Partner';
 
 @Injectable({
@@ -15,5 +19,21 @@ export class DataService {
 
   getAll(url: string): Observable<Array<Partners>> {
     return this._httpClient.get<Array<Partners>>(environment.rootApi + '/partners')
+      .pipe(
+        catchError(this.handleError)
+      )
+  }
+
+  private handleError(error: HttpErrorResponse) {
+
+    if (error.status === 404) {
+      return throwError(new NotFoundError(error))
+    }
+
+    if (error.status === 400) {
+      return throwError(new BadRequest(error))
+    }
+
+    return throwError(new AppError(error))
   }
 }
