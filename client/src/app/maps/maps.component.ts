aa58632cc0a55dfbf802bc4052fae0b3df7a472b
Rami Kanaan <kanaan95@live.com>
2021-06-06 15:25:09 +0400

    Google Maps options Added options for map + marker + circle

--- client/src/app/maps/maps.component.ts
+++ client/src/app/maps/maps.component.ts
@@ -9,6 +9,39 @@ export class MapsComponent implements OnInit {
 
   constructor() { }
 
+  google: any;
+
+  // Google Maps options
+  options: google.maps.MapOptions = {
+    center: { lat: 51.5144636, lng: -0.142571 },
+    zoom: 10,
+    fullscreenControl: false,
+    maxZoom: 18,
+    minZoom: 4,
+    streetViewControl: false
+  };
+
+  // Google Maps Markers options
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    optimized: true,
+  };
+
+  // Markers positions
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  // Google Maps Circle drawing options
+  circleOptions: google.maps.CircleOptions = {
+    center: { lat: 51.5144636, lng: -0.142571 },
+    radius: 0,
+    fillColor: "#2ecc71",
+    fillOpacity: .5,
+    editable: false,
+    strokeWeight: 1,
+    strokeColor: "#27ae60"
+  }
+
   ngOnInit(): void {
   }
 
