e8773642db7f6a5b7f6210fd1c1c79f1712646e4
Rami Kanaan <kanaan95@live.com>
2021-06-06 15:55:48 +0400

    Serivce  + Environement Added ngOnInit in AppComp Added rootApi env in environment file

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  constructor(private _dataService: DataService) { }
+
+  ngOnInit(): void {
+    this._dataService.getAll('/partners')
+  }
 }
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  getAll(url: string) {
+    return this._httpClient.get(environment.rootApi + '/partners').subscribe(
+      res => console.log(res)
+    )
+  }
 }
