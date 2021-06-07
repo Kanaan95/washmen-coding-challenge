92ff8d2a0effe69012818959ef656a2ad137837c
Rami Kanaan <kanaan95@live.com>
2021-06-07 13:12:08 +0400

    PWA added pwa

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { PartnersModule } from './partners/partners.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SearchModule } from './search/search.module';
 import { MapsService } from './services/maps.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,13 @@ import { MapsService } from './services/maps.service';
     MapsModule,
     PartnersModule,
     SearchModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [DataService, MapsService, { provide: ErrorHandler, useClass: AppErrorHandler }],
   bootstrap: [AppComponent]
